bd2bf70ecd81af655bbf67b5c55905a67763bb4b
wangw <357458468@qq.com>
2017-11-29 20:19:31 +0800

    test git

--- src/main/java/cn/wangw/App.java
+++ src/main/java/cn/wangw/App.java
@@ -2,10 +2,10 @@ package cn.wangw;
 
 /**
  * Hello world!
- *
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("test git");
     }
 }
